f858daf9cc932407a60fb78fb927b4c60bc1845b
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-01-21 22:44:13 +0200

    Commit

---
+++ Exercise/SumOfChars_04.java
@@ -0,0 +1,17 @@
+package DataTypesAndVariables.Exercise;
+
+import java.util.Scanner;
+
+public class SumOfChars_04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int charTimes = Integer.parseInt(scanner.nextLine());
+        int total = 0;
+        for (int i = 1; i <= charTimes; i++) {
+            char currentChar = scanner.nextLine().charAt(0);
+            total += currentChar;
+        }
+        System.out.printf("The sum equals: %d", total);
+    }
+}
