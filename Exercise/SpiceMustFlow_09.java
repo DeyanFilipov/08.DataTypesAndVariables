1a93171526425274e218a56ca16a83aefe33dacc
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-01-23 16:10:23 +0200

    Commit

---
+++ Exercise/SpiceMustFlow_09.java
@@ -0,0 +1,34 @@
+package DataTypesAndVariables.Exercise;
+
+import java.util.Scanner;
+
+public class SpiceMustFlow_09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int yield = Integer.parseInt(scanner.nextLine());
+        int remainingSpice = yield;
+        int workedDays = 0;
+        int workersDailyEat = 26;
+
+        while (yield >= 100) {
+            workedDays++;
+            if (workedDays > 1) {
+                remainingSpice += yield - workersDailyEat;
+            } else {
+                remainingSpice -= workersDailyEat;
+            }
+            if (remainingSpice < 0) {
+                remainingSpice = 0;
+            }
+            yield -= 10;
+        }
+        if (workedDays == 0) {
+            remainingSpice = 0;
+        } else {
+            remainingSpice -= 26;
+        }
+        System.out.println(workedDays);
+        System.out.println(remainingSpice);
+    }
+}
