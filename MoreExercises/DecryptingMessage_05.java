d048d40a98e09e2681da505f36a770fc85047153
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-01-23 22:32:36 +0200

    Commit

---
+++ MoreExercises/DecryptingMessage_05.java
@@ -0,0 +1,23 @@
+package DataTypesAndVariables.MoreExercises;
+
+import java.util.Scanner;
+
+public class DecryptingMessage_05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int key = Integer.parseInt(scanner.nextLine());
+        int num = Integer.parseInt(scanner.nextLine());
+        String decryptedText = "";
+        char lastChar = 0;
+
+        for (int i = 1; i <= num; i++) {
+            char currentChar = scanner.nextLine().charAt(0);
+            for (char j = currentChar; j <= currentChar + key; j++) {
+                lastChar = j;
+            }
+            decryptedText += lastChar;
+        }
+        System.out.println(decryptedText);
+    }
+}
