6978e3d8f0ef2afaf3ce50025fd04a329259d621
DeyanFilipov <didkofilipov.sv@gmail.com>
2023-01-23 15:45:19 +0200

    Commit

---
+++ Exercise/BeerKegs_08.java
@@ -0,0 +1,33 @@
+package DataTypesAndVariables.Exercise;
+
+import java.util.Scanner;
+
+public class BeerKegs_08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int input = Integer.parseInt(scanner.nextLine());
+        String winner = "";
+        float max = 0;
+        for (int i = 1; i <= input; i++) {
+            String model = scanner.nextLine();
+            String radius = scanner.nextLine();
+            String height = scanner.nextLine();
+            float current = bigger(radius, height);
+            if (current > max) {
+                max = current;
+                winner = model;
+            }
+        }
+        System.out.println(winner);
+    }
+
+    public static float bigger(String radius, String height) {
+        float convertedRadius = Float.parseFloat(radius);
+        int convertedHeight = Integer.parseInt(height);
+
+        float volume = (Float.parseFloat(String.valueOf(Math.PI))) * (Float.parseFloat(String.valueOf(Math.pow(convertedRadius, 2)))) * convertedHeight;
+        return volume;
+    }
+}
+
